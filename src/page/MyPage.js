610dce11377f35b1f04e2588f735bc57e8d1078c
usernameffas <151457809+usernameffas@users.noreply.github.com>
2026-03-15 16:44:16 +0900

    er

--- src/page/MyPage.js
+++ src/page/MyPage.js
@@ -10,11 +10,23 @@ const MyPage = () => {
   const dispatch = useDispatch();
 
   //오더리스트 들고오기
+  const { orders } = useSelector((state) => state.order);
 
+  useEffect(() => {
+    dispatch(orderActions.getOrderList());
+}, []);
   // 오더리스트가 없다면? 주문한 상품이 없습니다 메세지 보여주기
   return (
     <Container className="status-card-container">
-      <OrderStatusCard />
+     {orders && orders.length > 0 ? (
+  orders.map((order) => (
+     <OrderStatusCard key={order._id} order={order} />
+    ))
+  ) : (
+    <div className="text-center mt-5">
+      <h4>주문한 상품이 없습니다</h4>
+    </div>
+  )}
     </Container>
   );
 };
